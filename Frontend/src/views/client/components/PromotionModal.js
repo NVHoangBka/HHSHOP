e7a5ede1035c0994fe92cf398443b8017cb358d2
NVHoangBka <hoangdo.bka@gmail.com>
2026-03-04 18:09:57 +0700

    04/3/0226

--- Frontend/src/views/client/components/PromotionModal.js
+++ Frontend/src/views/client/components/PromotionModal.js
@@ -1,32 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { Modal } from "react-bootstrap";
+import React, { useEffect, useRef } from "react";
+import Modal from "bootstrap/js/dist/modal";
 import imgPromo from "../../../assets/image/promo/promo.png";
 
 const PromotionModal = () => {
-  const [show, setShow] = useState(false);
+  const modalRef = useRef(null);
 
   useEffect(() => {
     const hasSeen = localStorage.getItem("promoSeen");
 
     if (!hasSeen) {
       setTimeout(() => {
-        setShow(true);
+        const modal = new Modal(modalRef.current);
+        modal.show();
         localStorage.setItem("promoSeen", "true");
       }, 1000);
     }
   }, []);
 
   return (
-    <Modal show={show} onHide={() => setShow(false)} centered backdrop="true">
-      <Modal.Body className="p-0 position-relative">
-        <button
-          className="btn-close position-absolute top-0 end-0 m-3"
-          onClick={() => setShow(false)}
-        ></button>
+    <div
+      className="modal fade"
+      tabIndex="-1"
+      ref={modalRef}
+      style={{ zIndex: "3000" }}
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content border-0">
+          <div className="modal-body p-0 position-relative">
+            <button
+              type="button"
+              className="btn-close position-absolute top-0 end-0 m-3"
+            ></button>
 
-        <img src={imgPromo} alt="Khuyến mãi" className="img-fluid rounded" />
-      </Modal.Body>
-    </Modal>
+            <img
+              src={imgPromo}
+              alt="Khuyến mãi"
+              className="img-fluid rounded"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
