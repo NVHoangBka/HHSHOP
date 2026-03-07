0bf8593ef7594978863d3fb8c9d560dc594c0ce6
HoangHai <hoangdo.bka@gmail.com>
2026-03-07 11:05:09 +0700

    7/3/2026

--- Backend/routes/Payment.js
+++ Backend/routes/Payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { generateQR } = require("../controllers/paymentController");
+const { generateQR } = require("../controllers/PaymentController");
 const { protect } = require("../middleware/auth");
 
 router.post("/generate-qr", protect, generateQR);
